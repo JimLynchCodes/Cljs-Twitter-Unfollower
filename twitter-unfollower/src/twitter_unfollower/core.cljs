d9f0a6e6b0e75d19209297d6c701216003a4f0ea
JimTheMan <mrdotjim@gmail.com>
2018-03-06 08:51:42 -0500

    converting array to map

--- twitter-unfollower/src/twitter_unfollower/core.cljs
+++ twitter-unfollower/src/twitter_unfollower/core.cljs
@@ -9,10 +9,14 @@
             [cljs.core.async :refer [put! chan <!]]))
 
 
+(defn filterfn [followingMe followedByMe]
+  (println [followingMe followedByMe]))
+
 (defn getData []
 
   (def followingMeChan (chan))
   (def followedByMeChan(chan))
+  (def followedByMeDetailsChan(chan))
 
   (def config
     (-> (nodejs/require "fs")
@@ -29,15 +33,56 @@
 
   (.get Twitter "followers/ids" queryParams
         (fn [err data]
-          (put! followingMeChan (count (.-ids data)))))
+
+          (def mapOfIds (apply array-map
+                 (interleave (map keyword (.-ids data))
+                             (map (fn [x] 0) (.-length (.-ids data))))))
+          (println mapOfIds)
+
+          (put! followingMeChan (.-ids data))))
 
   (.get Twitter "friends/ids" queryParams
         (fn [err data]
-          (put! followedByMeChan (count (.-ids data))))))
+          (put! followedByMeDetailsChan (.-ids data)))))
+
+;  (.get Twitter "friends/list" queryParams
+;    (fn [err data]
+;
+;      (println "got friends list" data)
+;      (println "got friends list 2" (.-users (clj->js data)))
+;      (println "length" (.-length (.-users (clj->js data)))))
+;;      (println "got friends list" (first (:users data)))
+;;      (println "got friends list" (clj->js data))
+;;      (put! followedByMeChan (.-ids data)
+;
+;            ))
 
 (deflambda run-lambda [args ctx]
   (getData)
   (go
-    (let [followingMe  (<! followingMeChan)
-          followedByMe (<! followedByMeChan)]
+    (let [followingMe                   (<! followingMeChan)
+          followedByMe                  (<! followedByMeChan)
+;          followedByMeDetails           (<! followedByMeDetailsChan)
+          followedByMeButNotFollowingMe (filterFn followingMe followedByMe)]
+
+      (def unfollowChan(chan))
+
+;      (println followedByMeDetails)
+;      (println "down here")
+      (def unfollowParams
+        {:name        ""
+         :screen_name ""
+         :userId      ""})
+
+      (.post Twitter "friendships/destroy" queryParams
+             (fn [err data]
+;               (println "destroyed? " data)
+               (put! unfollowChan data)))
+
+
+      (<! unfollowChan)
+
+      (println "type " (type (first followingMe)))
+
       (ctx/succeed! ctx {:followingMe followingMe :followedByMe followedByMe}))))
+
